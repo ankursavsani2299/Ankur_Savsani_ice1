5e2b25da45b476366ec76eb0c2838ac5ee61954f
ankursavsani2299 <ankursavsani22@gmail.com>
2020-01-30 05:52:34 +0530

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/cardgameweek4/Card.java
+++ src/cardgameweek4/Card.java
@@ -5,14 +5,15 @@ package cardgameweek4;
  * @author ankur, 2020
  */
 public class Card {
-    
+
         public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
         public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+        public enum Joker {JOCKER1,JOCKER2 };
         private final Suit suit;
         private final Value value;
+        private Joker joker;
         
-        public Card(Suit s, Value gVal)
-        {
+        public Card(Suit s, Value gVal){
            suit =s;
            value= gVal;
         }
@@ -23,4 +24,8 @@ public class Card {
 	public Suit getSuit() {
 		return this.suit;
         }
+        public Joker getJoker() {
+		return this.joker;
+	}
 }
+        
